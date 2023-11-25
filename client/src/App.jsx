b8e32b54b811f2cfb18d71b0bb0a0771385990d7
milan_lazarevic <mlazarevic662@gmail.com>
2023-11-25 12:28:23 +0100

    Navbar setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Button } from "@material-tailwind/react";
+import Navbar from './components/Navbar';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -26,27 +27,7 @@ function App() {
 
   return (
     <div className="App ">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <Button color="blue">Button</Button>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Navbar />
     </div>
   )
 }
