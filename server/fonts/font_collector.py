e819dcb97b1f2a23031fa1cd30826bde064b88c3
Nikola Jolović <nikolajolovic02@gmail.com>
2023-11-26 18:58:31 +0100

    Uncomment call to match_fonts

--- server/fonts/font_collector.py
+++ server/fonts/font_collector.py
@@ -38,7 +38,7 @@ class FontCollector:
     def start_collector(self, GOOGLE_FONTS_API_KEY, font_service):
         self.fetch_fonts(GOOGLE_FONTS_API_KEY)
         self.save_fonts()
-        #match_fonts(font_service)
+        match_fonts(font_service)
 
     def task_start_font_collector(self, font_service):
         self.start_collector(self.google_fonts_api_key, font_service)
