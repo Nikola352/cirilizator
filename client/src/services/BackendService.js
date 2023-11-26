b9a6c0f7fd53c2e3eb8de79cec4af78fb0034c74
mrmi <nikola.31023@gmail.com>
2023-11-26 23:58:57 +0100

    Success message

--- client/src/services/BackendService.js
+++ client/src/services/BackendService.js
@@ -49,6 +49,35 @@ class BackendService {
             throw error;
         }
     }
+
+    static async createBlogPost(postData, jwtToken) {
+        try {
+            const apiEndpoint = import.meta.env.VITE_API_ENDPOINT;
+
+            if (!apiEndpoint) {
+                throw new Error('API endpoint not specified in the environment variables.');
+            }
+
+            const response = await fetch(apiEndpoint + 'posts', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${jwtToken}`, // Injecting JWT token
+                },
+                body: JSON.stringify(postData),
+            });
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+
+            const data = await response.json();
+            return data;
+        } catch (error) {
+            console.error('Error:', error);
+            throw error;
+        }
+    }
 }
 
 export default BackendService;
