2d1f72cb4466d5f031ff187f15443f5e980c72c8
milan_lazarevic <mlazarevic662@gmail.com>
2023-11-26 23:59:20 +0100

    recenica

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -6,16 +6,15 @@ export default function Banner() {
         <div className="banner my-24  max-w-[1150px] mx-auto">
             <div className="header-container my-8 ">
                 <p className="fontUsed text-primary-100">Poppins</p>
-                <h1 className="heading text-[7rem] font-thin">Погледајте шта знамо</h1>
+                <h1 className="heading lg:text-[6rem] text-6xl leading-[4rem] lg:leading-[6rem] font-thin">Савети за употребу ћирилице на интернету</h1>
             </div>
             <div className="paragraph-container my-4">
                 <p className="fontUsed text-primary-100 my-4">Times-New-Roman</p>
-                <p className="paragraph max-w-3xl">Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-                    Consequuntur earum nostrum eos optio ea nam ducimus 
-                    repellendus, aut voluptatibus quam inventore esse 
-                    quos fuga, explicabo eius excepturi id rerum. Veniam?</p>
+                <p className="paragraph max-w-3xl">Добродошли на сајт који вам пружа корисне 
+                савете о правилној употреби ћирилице на интернету. Сазнајте како боље представити 
+                ваш садржај користећи ћириличко писмо и како олакшати комуникацију са посетиоцима.</p>
             </div>
-            <CTA text="Погледајте" />
+            <CTA text="Погледајте" path={"/magazin"} />
         </div>
     )
 }
