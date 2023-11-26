f6d056222cec671d77d35152d3966a57419403f3
Nikola Jolović <nikolajolovic02@gmail.com>
2023-11-26 19:57:58 +0100

    Add useFetch hook

---
+++ client/src/hooks/useFetch.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react"
+
+const useFetch = (url) => {
+    const [data, setData] = useState(null)
+    const [isPending, setIsPending] = useState(true)
+    const [error, setError] = useState(null)
+    
+    useEffect(() => {
+        const fetchData = async () => {
+            try{
+                const res = await fetch(url)
+                if(res.ok){
+                    const data = await res.json()
+                    setData(data)
+                    setIsPending(false)
+                    setError(null)
+                } else{
+                    throw Error("Could not fetch data")
+                }
+            } catch(err){
+                setError(err.message)
+                setIsPending(false)
+            }
+        }
+        setTimeout(() => {
+            fetchData()
+        }, 300)
+    }, [url]);
+
+    return {data, isPending, error}
+}
+
+export default useFetch;
